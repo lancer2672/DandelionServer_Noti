b0ed46a1d8c24597725618648612834567ec161f
lancer2672 <jbkhanhtran@gmail.com>
2024-03-22 07:38:56 +0700

    add server addr

--- utils/config.go
+++ utils/config.go
@@ -3,8 +3,9 @@ package utils
 import "github.com/spf13/viper"
 
 type Config struct {
-	RABBITMQ_CONN string `mapstructure:"RABBITMQ_CONN"`
-	DB_SOURCE     string `mapstructure:"DB_SOURCE"`
+	RABBITMQ_CONN  string `mapstructure:"RABBITMQ_CONN"`
+	DB_SOURCE      string `mapstructure:"DB_SOURCE"`
+	SERVER_ADDRESS string `mapstructure:"SERVER_ADDRESS"`
 }
 
 // overrided by env if exists
--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,20 @@
 package utils
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func FailOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
+	}
+}
+func JSONResponse(res http.ResponseWriter, data interface{}, statusCode int) {
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(statusCode)
+	if data != nil {
+		json.NewEncoder(res).Encode(data)
 	}
 }
